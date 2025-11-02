5df394f55c1bd7a14bcff8d74bb806c5b0f3e856
mayankmishra1995 <mm884209@gmail.com>
2025-11-02 21:47:14 +0000

    refactor: add negative check and raise exception

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,5 +1,8 @@
 class StringCalculator
   def self.add numbers
-    numbers.split(/,|\n/).map(&:to_i).sum
+    parts = numbers.split(/,|\n/)
+    negatives = parts.select { |p| p.to_i < 0 }
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
+    parts.map(&:to_i).sum
   end
 end
