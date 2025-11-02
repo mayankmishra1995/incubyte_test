5a26bdf63015349a800dd5573d17b06b24b72728
mayankmishra1995 <mm884209@gmail.com>
2025-11-02 21:24:31 +0000

    spec: add failing test for single number

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe StringCalculator, type: :service do
       it 'returns 0 for empty string' do
         expect(StringCalculator.add('')).to eq 0
       end
+
+      it 'returns the number for single number' do
+        expect(StringCalculator.add('1')).to eq 1
+      end
     end
   end
 end
